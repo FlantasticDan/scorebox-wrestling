eb0ba997a0ef0aa0b0809c9518b7b03008677e3d
Daniel Flanagan <dancharley8@gmail.com>
2021-12-08 01:46:15 -0800

    Volleyball to Wrestling Port Framwork

---
+++ flask/manager.py
@@ -0,0 +1,86 @@
+from secrets import token_hex
+from threading import Thread
+from typing import Dict
+
+import socketio
+import httpx
+
+from consoles.sports import Wrestling
+
+def is_string_ip(string: str) -> bool:
+    octets = string.split('.')
+    return len(octets) == 4
+
+class WrestlingManager:
+    '''Wrestling Game State Manager'''
+    def __init__(self, home_team, home_mascot, home_color, visitor_team, visitor_mascot, visitor_color, com_port) -> None:
+        self.nonce = token_hex(6)
+
+        self.home_name = home_team
+        self.home_mascot = home_mascot
+        self.home_color = home_color
+        self.visitor_name = visitor_team
+        self.visitor_mascot = visitor_mascot
+        self.visitor_color = visitor_color
+
+        self.game_state = None # type: Dict
+
+        self.alert_mode = 'neutral'
+        self.alert_visibility = 'off'
+        self.alert_text = ''
+        self.display_mode = 'live'
+
+        self.remote = is_string_ip(com_port)
+        self.source = com_port
+
+        if not self.remote:
+            self.client = socketio.Client()
+            self.client_thread = Thread(target=self.socket_client)
+            self.client_thread.start()
+
+            self.console = Wrestling(com_port)
+            self.console.on_update = self.updater
+    
+        else:
+            httpx.get(f'http://{self.source}:9876/init/wrestling')
+
+    def updater(self, game_state):
+        if self.client.connected:
+            self.client.emit('update', game_state)
+            self.game_state = game_state
+        
+    def socket_client(self):
+        self.client.connect('http://localhost:5000')
+        self.client.wait()
+
+    def overlay_export(self) -> Dict:
+        return {
+            'home_name': self.home_name,
+            'home_mascot': self.home_mascot,
+            'home_color': self.home_color,
+            'visitor_name': self.visitor_name,
+            'visitor_mascot': self.visitor_mascot,
+            'visitor_color': self.visitor_color,
+        }
+    
+    def status_export(self) -> Dict:
+        return {
+            'alert_mode': self.alert_mode,
+            'alert_visibility': self.alert_visibility,
+            'alert_text': self.alert_text,
+            'display_mode': self.display_mode
+        }
+
+    def set_alert_mode(self, new_status) -> None:
+        self.alert_mode = new_status
+    
+    def set_alert_visibility(self, new_status) -> None:
+        self.alert_visibility = new_status
+    
+    def set_alert_text(self, new_status) -> None:
+        self.alert_text = new_status
+        self.alert_visibility = 'on'
+    
+    def set_display_mode(self, new_mode) -> None:
+        self.display_mode = new_mode
+    
---
+++ flask/static/overlay.js
@@ -0,0 +1,119 @@
+const scorebox = document.getElementById('scorebox')
+
+const homeScore = document.getElementById('home-score')
+const visitorScore = document.getElementById('visitor-score')
+const setScores = document.getElementById('set-score')
+const currentSet = document.getElementById('current-set')
+
+const homeSummaryScore = document.getElementById('home-summary-score')
+const visitorSummaryScore = document.getElementById('visitor-summary-score')
+const summaryTag = document.getElementById('summary-tag')
+
+const scorestate = document.getElementById('scorestate')
+
+const homeAlert = document.getElementById('home-alert')
+const visitorAlert = document.getElementById('visitor-alert')
+const neutralAlert = document.getElementById('neutral-alert')
+
+let statusObject = undefined
+let homeSets = 0
+let visitorSets = 0
+
+const socket = io()
+
+socket.on('connect', () => {
+    socket.emit('status-request', 'status')
+})
+
+socket.on('status', payload => {
+    statusObject = payload
+    StatusUpdate()
+})
+
+function StatusUpdate() {
+    if (statusObject.alert_visibility == 'off')
+    {
+        HideAlerts()
+    }
+    else {
+        if (statusObject.alert_visibility == 'on')
+        {
+            ProcessAlert()
+        }
+    }
+    
+    ProcessDisplayMode()
+}
+
+function HideAlerts() {
+    visitorAlert.classList.add('hidden')
+    homeAlert.classList.add('hidden')
+    neutralAlert.classList.add('hidden')
+}
+
+function ProcessAlert() {
+    HideAlerts()
+    switch (statusObject.alert_mode)
+    {
+        case 'home':
+            homeAlert.innerText = statusObject.alert_text
+            homeAlert.classList.remove('hidden')
+            break
+        case 'visitor':
+            visitorAlert.innerText = statusObject.alert_text
+            visitorAlert.classList.remove('hidden')
+            break
+        case 'neutral':
+            neutralAlert.innerText = statusObject.alert_text
+            neutralAlert.classList.remove('hidden')
+            break
+        default:
+            break
+    }
+}
+
+function ScoreBoxOut() {
+    scorebox.classList.add('out')
+    neutralAlert.classList.add('out')
+    homeAlert.classList.add('out')
+    visitorAlert.classList.add('out')
+
+    scorestate.classList.remove('out')
+}
+
+function ScoreBoxIn() {
+    scorebox.classList.remove('out')
+    neutralAlert.classList.remove('out')
+    homeAlert.classList.remove('out')
+    visitorAlert.classList.remove('out')
+
+    scorestate.classList.add('out')
+}
+
+function ProcessDisplayMode() {
+    homeSummaryScore.innerText = homeSets
+    visitorSummaryScore.innerText = visitorSets
+
+    if (statusObject.display_mode == 'live')
+    {
+        ScoreBoxIn()
+    }
+    else
+    {
+        switch (statusObject.display_mode)
+        {
+            case 'start':
+                summaryTag.innerText = 'Starting Soon'
+                break
+            case 'team':
+                summaryTag.innerText = 'Team Score'
+                break
+            case 'final':
+                summaryTag.innerText = 'Final'
+                break
+            default:
+                break
+        }
+        ScoreBoxOut()
+    }
+}
